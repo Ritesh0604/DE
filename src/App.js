cedb72ac620b24833fd6e3cbe41f64ae4640e74f
Rits0631 <94890637+Ritesh0604@users.noreply.github.com>
2023-09-03 23:50:25 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function App() {
+    return (
+        <React.Fragment>
+            <MainHeader/>
+            <main>
+                <Home/>
+            </main>
+        </React.Fragment>
+    );
+}
+
+export default App;
---
+++ src/components/MainHeader/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import classes from './Navigation.module.css';
+
+const Navigation = (props) => {
+    return (
+        <nav className={classes.nav}>
+                        <button>Navigate</button>
+            {/* <ul>
+                    <li>
+                    </li>
+            </ul> */}
+        </nav>
+    );
+};
+
+export default Navigation;
