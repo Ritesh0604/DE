8b6581afdffa67b911096578aaea2966b76c1651
Rits0631 <94890637+Ritesh0604@users.noreply.github.com>
2023-09-12 10:46:04 +0530

    delete deleteslice

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './authSlice'
-import deleteReducer from './deleteSlice'
 export default configureStore({
     reducer: {
         auth: authReducer,
-        delete: deleteReducer
     }
 })
