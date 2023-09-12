103c82763e4d258eca08726072040a51162cfe25
Khushipatel31 <khushipatel200431@gmail.com>
2023-09-12 10:53:12 +0530

    faculty card

--- src/components/Department/Department.js
+++ src/components/Department/Department.js
@@ -1,10 +1,77 @@
 import { useParams } from "react-router-dom";
-const Department=()=>{
-    const params=useParams();
-    return(
+import './Department.css'
+const Department = () => {
+    const params = useParams();
+    const teacher = {
+        name: "hiii",
+        note: "this is note"
+    }
+
+
+    return (
         <>
-        <h1>Department Page</h1>
-        <p>{params.navigateBlock}</p>
+            <h1>Department Page</h1>
+            <p>{params.navigateBlock}</p>
+            <div className="allFaculty row ">
+                <div class="card abc col-3" >
+                    <img class="card-img-top" src="/jrd.png" alt="Card image cap"></img>
+                    <div class="card-body">
+                        <div className="q">
+                            <label>Name:</label>
+                            <p>{teacher.name}</p>
+                        </div>
+                        <div className="q">
+                            <label>Note:</label>
+                            <p>{teacher.note}</p>
+                        </div>
+
+
+                    </div>
+                </div> <div class="card abc col-3" >
+                    <img class="card-img-top" src="/jrd.png" alt="Card image cap"></img>
+                    <div class="card-body">
+                        <div className="q">
+                            <label>Name:</label>
+                            <p>{teacher.name}</p>
+                        </div>
+                        <div className="q">
+                            <label>Note:</label>
+                            <p>{teacher.note}</p>
+                        </div>
+
+
+                    </div>
+                </div> <div class="card abc col-3" >
+                    <img class="card-img-top" src="/jrd.png" alt="Card image cap"></img>
+                    <div class="card-body">
+                        <div className="q">
+                            <label>Name:</label>
+                            <p>{teacher.name}</p>
+                        </div>
+                        <div className="q">
+                            <label>Note:</label>
+                            <p>{teacher.note}</p>
+                        </div>
+
+
+                    </div>
+                </div>
+                <div class="card abc col-3" >
+                    <img class="card-img-top" src="/jrd.png" alt="Card image cap"></img>
+                    <div class="card-body">
+                        <div className="q">
+                            <label>Name:</label>
+                            <p>{teacher.name}</p>
+                        </div>
+                        <div className="q">
+                            <label>Note:</label>
+                            <p>{teacher.note}</p>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
