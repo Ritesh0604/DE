f212341d1330892dc3ea3c066b13f69d57023aab
Ishasanghani <102859564+Ishasanghani@users.noreply.github.com>
2023-09-26 12:24:56 +0530

    logo

--- src/components/MainNavigation/MainHeader.js
+++ src/components/MainNavigation/MainHeader.js
@@ -9,13 +9,18 @@ const MainHeader = (props) => {
     return (
         <>
             <header>
-                <nav className="navbar">
-                    <div className="container-fluid ">
-                        <h1 className="heading">Vishwakarama Government Engineering College</h1>
-                        {!checkVal && <NavLink to="login" > <button className="d-flex btn btn-outline-success" type="submit">Admin Login</button></NavLink>}
-                        {checkVal && <NavLink to="form" > <button className="d-flex btn btn-outline-success" type="submit">Add Faculty</button></NavLink>}
-                    </div>
-                </nav>
+                <div className="row bg-primary p-0 d-flex">
+                    
+                        <div className='col-md-6 col-12 d-flex'>
+                            <img src = "logo.png" alt="vishw"  style={{height:"90%",width:"20%"}}/>
+                              <h4 className="heading p-0 m-0 d-flex align-items-center">Vishwakarama Government Engineering College</h4>
+                        </div>
+                        <div className="col offset-md-4 d-flex align-items-center " >
+                        {!checkVal && <NavLink to="login" > <button className="  btn  btn-outline-success " type="submit">Admin Login</button></NavLink>}
+                        {checkVal && <NavLink to="form" > <button className=" btn btn-outline-success " type="submit">Add Faculty</button></NavLink>}
+                        </div>
+                </div>
+             
             </header>
         </>
     );
